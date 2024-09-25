bd64ab9dd1935bc15c15e15887f4e98e6320abc3
Artyom Leonov <6leonov@gmail.com>
2024-09-25 14:25:27 +0300

    feat: add theme toggle to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 
+import { ThemeToggle } from "@/components/theme-toggle";
+
 export function Header({ children }: { children: React.ReactNode }) {
   return (
     <header className="sticky top-0 z-50 border border-border/40 bg-background/60 py-2 backdrop-blur">
-      <div className="container flex items-center justify-between">
-        <Link href="/" className="font-medium">
-          PennyWise
-        </Link>
+      <div className="container grid grid-cols-[1fr,_auto,_auto] gap-4">
+        <h1 className="flex items-center">
+          <Link href="/" className="font-medium">
+            PennyWise
+          </Link>
+        </h1>
+
+        <ThemeToggle />
 
         <nav>{children}</nav>
       </div>
