88b773469ce0184adf9360ba827c63322d2cb256
Artyom Leonov <6leonov@gmail.com>
2024-10-21 14:15:53 +0300

    feat: add tabs

--- src/features/transactions/dashboard.tsx
+++ src/features/transactions/dashboard.tsx
@@ -1,43 +1,15 @@
 "use client";
 
-import { useEffect, useMemo } from "react";
-import { Label, Pie, PieChart } from "recharts";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { useEffect } from "react";
 import { toast } from "sonner";
 
-import { buttonVariants } from "@/components/ui/button";
-import {
-  ChartConfig,
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "@/components/ui/chart";
-import { cn } from "@/lib/utils";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import { AddTransactionForm } from "./add-transaction-form";
-import { columns } from "./columns";
-import { DataTable } from "./data-table";
+import { AllTab, ExpenseTab, IncomeTab } from "./tabs";
 import { useWallet } from "./use-wallet";
-
-const chartConfig = {
-  category: {
-    label: "Categories",
-  },
-  first: {
-    color: "hsl(var(--chart-1))",
-  },
-  second: {
-    color: "hsl(var(--chart-2))",
-  },
-  third: {
-    color: "hsl(var(--chart-3))",
-  },
-  fourth: {
-    color: "hsl(var(--chart-4))",
-  },
-  other: {
-    color: "hsl(var(--chart-5))",
-  },
-} satisfies ChartConfig;
 
 export function Dashboard({
   walletId,
@@ -46,8 +18,7 @@ export function Dashboard({
   walletId: number;
   className?: string;
 }) {
-  const { wallet, categories, error, addTransaction, isPending } =
-    useWallet(walletId);
+  const { wallet, error, addTransaction, isPending } = useWallet(walletId);
 
   useEffect(() => {
     if (error) {
@@ -55,138 +26,41 @@ export function Dashboard({
     }
   }, [error]);
 
-  const chartData = useMemo(() => {
-    const x = wallet?.transactions.reduce(
-      (acc, transaction) => {
-        const category = categories.find(
-          (category) => category.id === transaction.category_id,
-        );
-
-        if (!category) {
-          return acc;
-        }
-
-        const existingCategory = acc.find(
-          (item) => item.category === category.name,
-        );
-
-        if (existingCategory) {
-          existingCategory.amount += Number(transaction.amount);
-        } else {
-          acc.push({
-            category: category.name,
-            amount: Number(transaction.amount),
-          });
-        }
-
-        return acc;
-      },
-      [] as { category: string; amount: number; fill?: string }[],
-    );
-
-    x?.sort((a, b) => b.amount - a.amount);
-
-    if (x === undefined) {
-      return [];
-    }
-
-    if (x.length >= 1) {
-      x[0].fill = "var(--color-first)";
-    }
-
-    if (x.length >= 2) {
-      x[1].fill = "var(--color-second)";
-    }
-
-    if (x.length >= 3) {
-      x[2].fill = "var(--color-third)";
-    }
-
-    if (x.length >= 4) {
-      x[3].fill = "var(--color-fourth)";
-    }
-
-    if (x.length >= 5) {
-      x[4] = {
-        fill: "var(--color-other)",
-        category: "Other",
-        amount: x.slice(4).reduce((acc, item) => acc + item.amount, 0),
-      };
-    }
-
-    return x.slice(0, 5);
-  }, [categories, wallet?.transactions]);
-
-  const totalAmount = useMemo(() => {
-    return wallet?.transactions.reduce(
-      (acc, transaction) => acc + Number(transaction.amount),
-      0,
-    );
-  }, [wallet?.transactions]);
-
   return (
     <section className={className}>
-      <ChartContainer
-        config={chartConfig}
-        className="mx-auto aspect-square max-h-96"
-      >
-        <PieChart>
-          <ChartTooltip
-            cursor={false}
-            content={<ChartTooltipContent hideLabel />}
-          />
+      {isPending ? (
+        <Skeleton className="h-96 bg-secondary" />
+      ) : wallet && wallet.transactions.length > 0 ? (
+        <Tabs defaultValue="all">
+          <TabsList>
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="income">Income</TabsTrigger>
+            <TabsTrigger value="expense">Expense</TabsTrigger>
+          </TabsList>
 
-          <Pie
-            data={chartData}
-            dataKey="amount"
-            nameKey="category"
-            innerRadius={60}
-            strokeWidth={5}
-          >
-            <Label
-              content={({ viewBox }) => {
-                if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                  return (
-                    <text
-                      x={viewBox.cx}
-                      y={viewBox.cy}
-                      textAnchor="middle"
-                      dominantBaseline="middle"
-                    >
-                      <tspan
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        className="fill-foreground text-lg font-bold"
-                      >
-                        {totalAmount?.toLocaleString()} {wallet?.currency}
-                      </tspan>
+          <TabsContent value="all">
+            <AllTab wallet={wallet} />
+          </TabsContent>
 
-                      <tspan
-                        x={viewBox.cx}
-                        y={(viewBox.cy || 0) + 24}
-                        className="fill-muted-foreground"
-                      >
-                        Total
-                      </tspan>
-                    </text>
-                  );
-                }
-              }}
-            />
-          </Pie>
-        </PieChart>
-      </ChartContainer>
+          <TabsContent value="income">
+            <IncomeTab wallet={wallet} />
+          </TabsContent>
 
-      <div className="mt-8">
-        <AddTransactionForm
-          onSubmit={(values) => addTransaction(values)}
-          categories={categories.map((category) => category.name)}
-          isPending={isPending}
-          className={cn(buttonVariants({ variant: "outline" }))}
-        />
+          <TabsContent value="expense">
+            <ExpenseTab wallet={wallet} />
+          </TabsContent>
+        </Tabs>
+      ) : (
+        <Alert variant="destructive">
+          <MagnifyingGlassIcon />
+          <AlertTitle>No transactions</AlertTitle>
 
-        <DataTable columns={columns} data={wallet?.transactions || []} />
-      </div>
+          <AlertDescription>
+            No transactions found. Click the button below to add a new
+            transaction.
+          </AlertDescription>
+        </Alert>
+      )}
     </section>
   );
 }
---
+++ src/features/transactions/tabs/expense-tab.tsx
@@ -0,0 +1,127 @@
+import { useMemo } from "react";
+import { Label, Pie, PieChart } from "recharts";
+
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+import { chartConfig } from "../constants";
+import type { Wallet } from "../types";
+
+export function ExpenseTab({ wallet }: { wallet: Wallet }) {
+  const chartData = useMemo(() => {
+    const result = wallet.transactions.reduce(
+      (acc, transaction) => {
+        const amount = Number(transaction.amount);
+
+        if (amount > 0) {
+          return acc;
+        }
+
+        const categoryIndex = acc.findIndex(
+          ({ category }) => category === transaction.category_name,
+        );
+
+        if (categoryIndex !== -1) {
+          acc[categoryIndex].amount -= amount;
+        } else {
+          acc.push({
+            amount: -amount,
+            category: transaction.category_name,
+          });
+        }
+
+        return acc;
+      },
+      [] as { amount: number; category: string; fill?: string }[],
+    );
+
+    result.sort((a, b) => b.amount - a.amount);
+
+    if (result.length > 0) {
+      result[0].fill = "var(--first-color)";
+    }
+
+    if (result.length > 1) {
+      result[1].fill = "var(--second-color)";
+    }
+
+    if (result.length > 2) {
+      result[2].fill = "var(--third-color)";
+    }
+
+    if (result.length > 3) {
+      result[3].fill = "var(--fourth-color)";
+    }
+
+    if (result.length > 4) {
+      result[4].amount = result
+        .slice(4)
+        .reduce((acc, { amount }) => acc + amount, 0);
+
+      result[4].fill = "var(--other-color)";
+    }
+
+    return result.slice(0, 5);
+  }, [wallet.transactions]);
+
+  return (
+    <ChartContainer
+      config={chartConfig}
+      className="mx-auto aspect-square max-h-96"
+    >
+      <PieChart>
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent hideLabel />}
+        />
+
+        <Pie
+          data={chartData}
+          dataKey="amount"
+          nameKey="category"
+          innerRadius={60}
+          strokeWidth={5}
+        >
+          <Label
+            content={({ viewBox }) => {
+              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                return (
+                  <text
+                    x={viewBox.cx}
+                    y={viewBox.cy}
+                    textAnchor="middle"
+                    dominantBaseline="middle"
+                  >
+                    <tspan
+                      x={viewBox.cx}
+                      y={viewBox.cy}
+                      className="fill-foreground text-lg font-bold"
+                    >
+                      {new Intl.NumberFormat("de-DE", {
+                        style: "currency",
+                        currency: wallet.currency,
+                      }).format(
+                        chartData.reduce((acc, { amount }) => acc + amount, 0),
+                      )}
+                    </tspan>
+
+                    <tspan
+                      x={viewBox.cx}
+                      y={(viewBox.cy || 0) + 24}
+                      className="fill-muted-foreground"
+                    >
+                      Outcome
+                    </tspan>
+                  </text>
+                );
+              }
+            }}
+          />
+        </Pie>
+      </PieChart>
+    </ChartContainer>
+  );
+}
