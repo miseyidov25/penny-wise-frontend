46b89deca5709bafc442a9e060eb58678f7f6914
Artyom Leonov <6leonov@gmail.com>
2024-10-24 20:00:07 +0300

    refactor: improve retrieval of XSRF token in axios interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import Cookies from "js-cookie";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
@@ -11,8 +10,13 @@ export const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const xsrfToken = Cookies.get("XSRF-TOKEN");
+  const xsrfToken = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("XSRF-TOKEN="))
+    ?.split("=")[1];
+
   console.log("xsrfToken", xsrfToken);
+
   if (xsrfToken) {
     config.headers["X-XSRF-TOKEN"] = xsrfToken;
   }
