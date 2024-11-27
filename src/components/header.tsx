afbc60adc0b8c161dea2c4d86674d5e0b43a60f4
Vladislavs Baranovs <98613566+Vandrepus@users.noreply.github.com>
2024-11-27 08:42:27 +0200

    feat: add user manual to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
+import { FileTextIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
 import { ThemeToggle } from "@/components/theme-toggle";
+
+import { buttonVariants } from "./ui/button";
 
 export function Header({
   children,
@@ -11,8 +14,8 @@ export function Header({
 }) {
   return (
     <header className="sticky top-0 z-50 border border-border/40 bg-background/60 py-2 backdrop-blur">
-      <div className="container grid grid-cols-[1fr,_auto,_auto] gap-4">
-        <h1 className="flex items-center">
+      <div className="container flex gap-4">
+        <h1 className="flex flex-grow items-center">
           <Link
             href={isAuthorized ? "/dashboard" : "/"}
             className="font-medium"
@@ -20,6 +23,13 @@ export function Header({
             PennyWise
           </Link>
         </h1>
+
+        <Link
+          href="user_manual.pdf"
+          className={buttonVariants({ size: "icon", variant: "outline" })}
+        >
+          <FileTextIcon />
+        </Link>
 
         <ThemeToggle />
 
